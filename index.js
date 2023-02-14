331a6c7ead959531b2feee031725e7e83e098513
Your Name <you@example.com>
2023-02-14 11:38:20 +0530

    update

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ app.use(express.json());
 const cors = require("cors");
 app.use(
   cors({
-    origin: ["technotechinfo.epizy.com"],
+    origin: ["http://technotechinfo.epizy.com"],
     credentials: true,
   })
 );
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "technotechinfo.epizy.com");
+  res.header("Access-Control-Allow-Origin", "http://technotechinfo.epizy.com");
   res.header("Access-Control-Allow-Methods", "GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token, x-refresh-token, _id,authorization");
 
